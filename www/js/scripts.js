c5d13c99a374e018fa9a9ce94d7698d1ed4e1f4e
Jaslee1 <jasleedesign@gmail.com>
2015-12-13 08:26:20 -0500

    CAMP299

---
+++ www/js/scripts.js
@@ -0,0 +1,73 @@
+jQuery(document).ready(function ($) {
+
+
+    $(window).stellar();
+
+    var links = $('.navigation').find('li');
+    slide = $('.slide');
+    button = $('.button');
+    mywindow = $(window);
+    htmlbody = $('html,body');
+
+
+    slide.waypoint(function (event, direction) {
+
+        dataslide = $(this).attr('data-slide');
+
+        if (direction === 'down') {
+            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').prev().removeClass('active');
+        }
+        else {
+            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').next().removeClass('active');
+        }
+
+    });
+ 
+    mywindow.scroll(function () {
+        if (mywindow.scrollTop() == 0) {
+            $('.navigation li[data-slide="1"]').addClass('active');
+            $('.navigation li[data-slide="2"]').removeClass('active');
+        }
+    });
+
+    function goToByScroll(dataslide) {
+        htmlbody.animate({
+            scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top
+        }, 2000, 'easeInOutQuint');
+    }
+
+
+
+    links.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+    });
+
+    button.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+
+    });
+
+
+});
+
+
+
+$(".emailimg").on('mouseover mouseout', function(event) {
+  if (event.type == 'mouseover') {
+    $(this).find('.emailaddress').slideDown(10);
+  } else {
+    $(this).find('.emailaddress').slideUp(10);
+  }
+});
+
+$(".cellimg").on('mouseover mouseout', function(event) {
+  if (event.type == 'mouseover') {
+    $(this).find('.cellnumber').slideDown(10);
+  } else {
+    $(this).find('.cellnumber').slideUp(10);
+  }
+});
